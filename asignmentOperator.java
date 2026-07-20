5f3b8068571c8dc16179206011698fab4708b4ca
Mayuri Suryakant Masale <masalemayuri01@gmail.com>
2026-07-20 12:50:06 +0530

    assignment_operator_program

---
+++ asignmentOperator.java
@@ -0,0 +1,28 @@
+class asignmentOperator{
+
+    public static void main (String[] args){
+        int num = 20;
+
+        System.out.println("Intial : " + num);
+
+
+        num += 5;
+        System.out.println("After +5 :" +num);
+
+        num *= 2;
+        System.out.println("After *2 : " +num);
+
+        num -=5;
+        System.out.println("After -5 : " +num);
+
+        num /=2;
+        System.out.println("After /2 : " +num);
+
+        num %=3;
+        System.out.println("After %3 : " +num);
+        
+    }
+    
+}
+// assignment operators are 
+// += , -= , %= , /= 
