cc58055885cf2ddd0e5da6f915d7ce79bfe09c7b
Mayuri Suryakant Masale <masalemayuri01@gmail.com>
2026-07-20 13:02:16 +0530

    relational operator program

---
+++ relationalOperator.java
@@ -0,0 +1,32 @@
+class relationalOperator {
+
+    public static void main(String [] main){
+
+        int x = 10;
+        int y = 20;
+        int z = 30;
+
+        System.out.println("x  > y :" +(x > y));
+        System.out.println("x  < y :" +(x < y));
+        System.out.println("x  >= y :" +(x >= y));
+        System.out.println("x  <= y :" +(x <= y));
+        System.out.println("x  == z :" +(x == z));
+        System.out.println("x  != z :" +(x != z));
+        
+    }
+}
+
+/*
+Relational Operators are used to check for relations 
+like equality, greater than, and less than. They return 
+boolean results after the comparison and are extensively
+used in looping statements as well as conditional if-else 
+ statements.  
+
+Explanation:
+
+Variables a, b, and c are compared using relational operators.
+ - Operators like >, <, >=, <=, ==, != return boolean values.
+ - hese comparisons help in decision-making (if-else, loops).
+ - Each expression prints either true or false based on the condition.
+ */
