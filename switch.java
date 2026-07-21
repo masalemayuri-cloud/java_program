5c40ec986eae3052f85815af100f0fb776ee51a4
Mayuri Suryakant Masale <masalemayuri01@gmail.com>
2026-07-21 22:05:24 +0530

    java_program

---
+++ switch.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class switchexample
+ {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your number in 5 muliple ");
+        int num = sc.Nextint();
+        switch(num)
+        {
+            case 5:
+                System.out.println("You Entered "+num);
+                break;
+            case 10:
+                System.out.println("You Entered "+num);
+                break;
+            case 15:
+                System.out.println("You Entered "+num);
+                break;
+            case 20:
+                System.out.println("You Entered "+num);
+                break;
+        }
+
+    }
+
+}
+    
+   
+
