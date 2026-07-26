61e566b878301743f113f760c7dbbec412ada68c
Mayuri Suryakant Masale <masalemayuri01@gmail.com>
2026-07-26 22:40:00 +0530

    java Encapsulation

---
+++ Encapsulation.java
@@ -0,0 +1,20 @@
+class programmer{
+
+    private String name;
+
+    public String getName(){return name;}
+
+    public void setName(String name){
+        this.name = name;
+    }
+}
+
+public class Encapsulation{
+
+    public static void main(String[] args){
+
+        programmer p = new programmer();
+        p.setName("Mayuri");
+        System.out.println("name : "+p.getName()); 
+    }
+}
